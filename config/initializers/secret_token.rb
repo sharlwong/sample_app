2c0ba3cd06bca3476ca3941ec5f46789eeebc945
Sharlene Wong <sharlene@tinkerbox.com.sg>
2013-06-25 16:42:09 +0800

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SampleApp::Application.config.secret_token = '7006c8bbdaa529a213f87bd6549faa8d96ac566cd011ac739ea5fe33e8d30a3f11d6f3a317c382a91bea4a5415541c0ea7abbe3026170071f0f7d1624464bd6e'
